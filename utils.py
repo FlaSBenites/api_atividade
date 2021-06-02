78a96d25dcc17c7c2a771a95277ddbba904a498a
Flávio <flaviosbs82@gmail.com>
2021-06-02 14:26:17 -0300

    Alterado REST API para realizar autenticação de usuario e senha e criação da tabela usuarios

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from models import Pessoas
+from models import Pessoas, Usuarios
 
 def insere_pessoas():
     pessoa = Pessoas(nome='Erica', idade=32)
@@ -21,8 +21,19 @@ def exclui_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Flavio').first()
     pessoa.delete()
 
+def insere_usuario(login,senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def consulta_todos_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
+
 if __name__ == '__main__':
+    insere_usuario('flavio','1234')
+    insere_usuario('erica','4321')
+    consulta_todos_usuarios()
     # insere_pessoas()
     # altera_pessoa()
-    exclui_pessoa()
+    # exclui_pessoa()
     consulta_pessoas()
